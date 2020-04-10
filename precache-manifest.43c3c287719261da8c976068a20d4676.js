a7686d94ced4c471d68b6502354ec4485d009792
Steven Ha <ming@Stevens-MacBook-Pro.local>
2020-04-10 05:00:28 +0100

    Updates

---
+++ precache-manifest.43c3c287719261da8c976068a20d4676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9549311aaee11e82e5a3140881bfe0c9",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "43ce536bf85747495754",
+    "url": "/MyPortfolio/static/js/2.d893915f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyPortfolio/static/js/2.d893915f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba5526aa57edcada79a",
+    "url": "/MyPortfolio/static/js/main.3a4b4b24.chunk.js"
+  },
+  {
+    "revision": "52a4bad007323f1d525b",
+    "url": "/MyPortfolio/static/js/runtime-main.90f6a591.js"
+  },
+  {
+    "revision": "8e56dee4b6f5bd14812c29f5d5c2a857",
+    "url": "/MyPortfolio/static/media/Selfie.8e56dee4.png"
+  }
+]);
